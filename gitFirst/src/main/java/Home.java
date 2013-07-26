ae58d193c973892b5a16f66cb8d5e46399a22980
nandan singh <nandan_singh@live.com>
2013-07-26 23:32:35 +0530

    my third commit

--- gitFirst/src/main/java/Home.java
+++ gitFirst/src/main/java/Home.java
@@ -4,6 +4,7 @@ public class Home {
 	public static void main(String[] args) {
 		
 		System.out.println("this is the first egit experience!!");
+		System.out.println("now I am changing the files.");
 	}
 
 }
--- gitFirst/src/test/java/HomeTest.java
+++ gitFirst/src/test/java/HomeTest.java
@@ -4,6 +4,7 @@ public class HomeTest {
 	public static void main(String[] args) {
 		
 		System.out.println("this is test case");
+		System.out.println("this is second test case");
 	}
 
 }
